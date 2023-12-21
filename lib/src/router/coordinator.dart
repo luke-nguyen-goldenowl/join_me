e0dcfac2d0eb1d183efef1e64ffd79fce124c336
kienvo3105 <keith.vo.goldenowl@gmail.com>
2023-12-21 16:03:54 +0700

    add function navigate

--- lib/src/router/coordinator.dart
+++ lib/src/router/coordinator.dart
@@ -29,7 +29,10 @@ class AppCoordinator {
   static void showAccountScreen() =>
       context.goNamed(AppRouteNames.account.name);
   static void showEventScreen() => context.goNamed(AppRouteNames.event.name);
-
+  static void showFollowedScreen() =>
+      context.goNamed(AppRouteNames.followed.name);
+  static void showManageScreen() =>
+      context.goNamed(AppRouteNames.manageEvent.name);
   // static Future<T?> showSignInScreen<T extends Object?>() =>
   //     context.pushNamed<T>(AppRouteNames.signIn.name);
 
@@ -42,6 +45,15 @@ class AppCoordinator {
 
   static Future<T?> showAddEventScreen<T extends Object?>() =>
       context.pushNamed<T>(AppRouteNames.addEvent.name);
+
+  static Future<T?> showNotifyScreen<T extends Object?>() =>
+      context.pushNamed<T>(AppRouteNames.notify.name);
+
+  static Future<T?> showSearchScreen<T extends Object?>() =>
+      context.pushNamed<T>(AppRouteNames.search.name);
+
+  static Future<T?> showAddStoryScreen<T extends Object?>() =>
+      context.pushNamed<T>(AppRouteNames.addStory.name);
 
   static Future<T?> showSignUpScreen<T extends Object?>() =>
       context.pushNamed<T>(AppRouteNames.signUp.name);
@@ -57,6 +69,38 @@ class AppCoordinator {
       context.pushNamed<T>(
         AppRouteNames.sampleDetails.name,
         pathParameters: {AppRouteNames.sampleDetails.paramName!: id},
+      );
+
+  static Future<T?> showEventDetails<T extends Object?>({required String id}) =>
+      context.pushNamed<T>(
+        AppRouteNames.detailEvent.name,
+        pathParameters: {AppRouteNames.detailEvent.paramName!: id},
+      );
+
+  static Future<T?> showStoryScreen<T extends Object?>({required String id}) =>
+      context.pushNamed<T>(
+        AppRouteNames.story.name,
+        pathParameters: {AppRouteNames.story.paramName!: id},
+      );
+  static Future<T?> showProfileOtherUser<T extends Object?>(
+          {required String id}) =>
+      context.pushNamed<T>(
+        AppRouteNames.profileOtherUser.name,
+        pathParameters: {AppRouteNames.profileOtherUser.paramName!: id},
+      );
+
+  static Future<T?> showEditEventScreen<T extends Object?>(
+          {required String id}) =>
+      context.pushNamed<T>(
+        AppRouteNames.editEvent.name,
+        pathParameters: {AppRouteNames.editEvent.paramName!: id},
+      );
+
+  static Future<T?> showManageEventDetails<T extends Object?>(
+          {required String id}) =>
+      context.pushNamed<T>(
+        AppRouteNames.manageEventDetail.name,
+        pathParameters: {AppRouteNames.manageEventDetail.paramName!: id},
       );
 
   static Future<T?> showProfile<T extends Object?>() =>
