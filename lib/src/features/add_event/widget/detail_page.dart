a22c3b3715dd8e9160d92612b2c64a91ffe52e74
kienvo3105 <keith.vo.goldenowl@gmail.com>
2023-12-27 17:20:32 +0700

    delete comment

--- lib/src/features/add_event/widget/detail_page.dart
+++ lib/src/features/add_event/widget/detail_page.dart
@@ -66,7 +66,6 @@ class DetailPage extends StatelessWidget {
                       labelText: "Maximum number of attendees",
                     ),
                   ),
-
                   XInput(
                     key: const Key('addEvent_startDateEventInput_textField'),
                     readOnly: true,
@@ -92,7 +91,6 @@ class DetailPage extends StatelessWidget {
                       }
                     },
                   ),
-
                   XInput(
                     key: const Key('addEvent_timeEventInput_textField'),
                     readOnly: true,
@@ -114,7 +112,6 @@ class DetailPage extends StatelessWidget {
                       }
                     },
                   ),
-
                   XInput(
                     key: const Key('addEvent_deadlineEventInput_textField'),
                     readOnly: true,
@@ -140,161 +137,6 @@ class DetailPage extends StatelessWidget {
                       }
                     },
                   ),
-
-                  // TextFormField(
-                  //   controller: state.nameController,
-                  //   style: const TextStyle(
-                  //     fontSize: 15,
-                  //   ),
-                  //   decoration: const InputDecoration(
-                  //     labelText: 'Name event',
-                  //     labelStyle: TextStyle(
-                  //       fontSize: 15,
-                  //       fontWeight: FontWeight.bold,
-                  //       color: AppColors.grey,
-                  //     ),
-                  //   ),
-                  //   validator: (value) {
-                  //     if (value!.isEmpty) {
-                  //       return 'Please enter a name event';
-                  //     }
-                  //     return null;
-                  //   },
-                  // ),
-                  // const SizedBox(height: 16.0),
-                  // TextFormField(
-                  //   controller: state.descriptionController,
-                  //   minLines: 1,
-                  //   maxLines: 5,
-                  //   style: const TextStyle(
-                  //     fontSize: 15,
-                  //   ),
-                  //   decoration: const InputDecoration(
-                  //     labelText: 'Description',
-                  //     labelStyle: TextStyle(
-                  //       fontSize: 15,
-                  //       fontWeight: FontWeight.bold,
-                  //       color: AppColors.grey,
-                  //     ),
-                  //   ),
-                  //   validator: (value) {
-                  //     if (value!.isEmpty) {
-                  //       return 'Please enter a description';
-                  //     }
-                  //     return null;
-                  //   },
-                  // ),
-                  // const SizedBox(height: 16.0),
-                  // TextFormField(
-                  //   controller: state.numberMemberController,
-                  //   style: const TextStyle(
-                  //     fontSize: 15,
-                  //   ),
-                  //   decoration: const InputDecoration(
-                  //     labelText: 'Maximum number of attendees',
-                  //     labelStyle: TextStyle(
-                  //       fontSize: 15,
-                  //       fontWeight: FontWeight.bold,
-                  //       color: AppColors.grey,
-                  //     ),
-                  //   ),
-                  //   keyboardType: TextInputType.number,
-                  //   validator: (value) {
-                  //     if (value!.isEmpty) {
-                  //       return 'Please enter the maximum number of members';
-                  //     }
-                  //     if (int.tryParse(value) == null) {
-                  //       return 'Please enter a valid number';
-                  //     }
-                  //     return null;
-                  //   },
-                  // ),
-                  // const SizedBox(height: 16.0),
-                  // TextFormField(
-                  //   style: const TextStyle(
-                  //     fontSize: 15,
-                  //   ),
-                  //   readOnly: true,
-                  //   controller: state.dateController,
-                  //   decoration: const InputDecoration(
-                  //     labelText: 'Start day',
-                  //     labelStyle: TextStyle(
-                  //       fontSize: 15,
-                  //       fontWeight: FontWeight.bold,
-                  //       color: AppColors.grey,
-                  //     ),
-                  //   ),
-                  //   onTap: () async {
-                  //     DateTime? selectedDate = await showDatePicker(
-                  //       context: context,
-                  //       initialDate: DateTime.now(),
-                  //       firstDate: DateTime.now(),
-                  //       lastDate: DateTime(2101),
-                  //     );
-                  //     if (selectedDate != null &&
-                  //         selectedDate != DateTime.now()) {
-                  //       state.dateController.text =
-                  //           DateFormat("dd/MM/yyyy").format(selectedDate);
-                  //     }
-                  //   },
-                  // ),
-                  // const SizedBox(height: 16.0),
-                  // TextFormField(
-                  //   style: const TextStyle(
-                  //     fontSize: 15,
-                  //   ),
-                  //   readOnly: true,
-                  //   controller: state.timeController,
-                  //   decoration: const InputDecoration(
-                  //     labelText: 'Time',
-                  //     labelStyle: TextStyle(
-                  //       fontSize: 15,
-                  //       fontWeight: FontWeight.bold,
-                  //       color: AppColors.grey,
-                  //     ),
-                  //   ),
-                  //   onTap: () async {
-                  //     TimeOfDay? selectedTime = await showTimePicker(
-                  //       context: context,
-                  //       initialTime: TimeOfDay.now(),
-                  //     );
-                  //     if (selectedTime != null &&
-                  //         selectedTime != TimeOfDay.now()) {
-                  //       state.timeController.text =
-                  //           // ignore: use_build_context_synchronously
-                  //           selectedTime.format(context);
-                  //     }
-                  //   },
-                  // ),
-                  // const SizedBox(height: 16.0),
-                  // TextFormField(
-                  //   style: const TextStyle(
-                  //     fontSize: 15,
-                  //   ),
-                  //   readOnly: true,
-                  //   controller: state.deadlineController,
-                  //   decoration: const InputDecoration(
-                  //     labelText: 'Registration expiration date',
-                  //     labelStyle: TextStyle(
-                  //       fontSize: 15,
-                  //       fontWeight: FontWeight.bold,
-                  //       color: AppColors.grey,
-                  //     ),
-                  //   ),
-                  //   onTap: () async {
-                  //     DateTime? selectedDate = await showDatePicker(
-                  //       context: context,
-                  //       initialDate: DateTime.now(),
-                  //       firstDate: DateTime.now(),
-                  //       lastDate: DateTime(2101),
-                  //     );
-                  //     if (selectedDate != null &&
-                  //         selectedDate != DateTime.now()) {
-                  //       state.deadlineController.text =
-                  //           DateFormat("dd/MM/yyyy").format(selectedDate);
-                  //     }
-                  //   },
-                  // ),
                 ],
               ),
             ],
