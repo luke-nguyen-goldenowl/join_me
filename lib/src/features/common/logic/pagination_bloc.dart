ad4c22f9aabd999aff25045005948555691fdbda
kienvo3105 <keith.vo.goldenowl@gmail.com>
2024-01-02 13:40:16 +0700

    update refresh pagination_bloc.dart

--- lib/src/features/common/logic/pagination_bloc.dart
+++ lib/src/features/common/logic/pagination_bloc.dart
@@ -1,35 +1,34 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:myapp/src/features/account/logic/account_bloc.dart';
-// import 'package:myapp/src/features/common/logic/m_cubit.dart';
-// import 'package:myapp/src/network/model/common/pagination.dart';
 import 'package:myapp/src/network/model/common/pagination/pagination.dart';
-// import 'package:myapp/src/network/model/common/pagination_model.dart';
+import 'package:myapp/src/network/model/common/pagination/pagination_response.dart';
 import 'package:myapp/src/network/model/common/result.dart';
 
-// abstract class PaginationBloc<T extends PaginationState> extends MCubit<T> {
-// PaginationBloc(super.initialState) {
-//   getData();
-// }
+abstract class PaginationBloc<T extends PaginationState> extends Cubit<T> {
+  PaginationBloc(super.initialState) {
+    getData();
+  }
 
-// Future<MResult<MPaginationModel>> get getDataAPI;
+  Future<MResult<MPaginationResponse>> get getDataAPI;
 
-// Future<void> getData() async {
-//   if (!GetIt.I<AccountBloc>().state.isLogin && state.needLogin) return;
-//   if (!state.data.canLoad) return;
+  Future<void> getData() async {
+    if (!GetIt.I<AccountBloc>().state.isLogin && state.needLogin) return;
+    if (!state.data.canLoad) return;
 
-//   emit(state.copyWith(data: state.data.toLoading()) as T);
+    emit(state.copyWith(data: state.data.toLoading()) as T);
 
-//   final result = await getDataAPI;
-//   emit(state.copyWith(data: state.data.addAllFromResult(result)) as T);
-// }
+    final result = await getDataAPI;
+    emit(state.copyWith(data: state.data.addAllFromResult(result)) as T);
+  }
 
-// Future<void> refreshData() async {
-//   if (state.data.isLoading) return;
-//   emit(state.copyWith(data: state.data.refresh()) as T);
-//   getData();
-// }
-// }
+  Future<void> refreshData() async {
+    if (state.data.isLoading) return;
+    emit(state.copyWith(data: MPagination()) as T);
+    getData();
+  }
+}
 
 abstract class PaginationState<T> extends Equatable {
   const PaginationState({
