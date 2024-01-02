018af3d91b43a636a11a6c5900f06ac876bb1833
Keith-Go <152830147+Keith-Go@users.noreply.github.com>
2024-01-02 13:38:15 +0700

    Update event_repository_mock.dart

--- lib/src/network/data/event/event_repository_mock.dart
+++ lib/src/network/data/event/event_repository_mock.dart
@@ -1,11 +1,75 @@
-import 'package:myapp/src/network/model/common/result.dart';
-import 'package:myapp/src/network/model/event/event.dart';
-import 'package:myapp/src/network/model/user/user.dart';
-import 'package:myapp/src/network/model/user_event/user_event.dart';
+List<MEvent> events = [
+  MEvent(
+    id: '1',
+    name: "Happy birthday",
+    description: "this is description",
+    images: ["assets/images/images/bg-event.jpg"],
+    startDate: DateTime.now(),
+    deadline: DateTime.now(),
+    province: 'Ho Chi Minh',
+    location: LatLng(88.00015, 85.1316546),
+    host: const MUser(
+        id: '1', name: 'Keith', avatar: "assets/images/images/avatar.png"),
+    follower: 122,
+    type: 'sport',
+  ),
+  MEvent(
+    id: '2',
+    name: "Halloween",
+    description: "this is description",
+    images: ["assets/images/images/bg-event.jpg"],
+    startDate: DateTime.now(),
+    deadline: DateTime.now(),
+    province: 'Ho Chi Minh',
+    location: LatLng(88.00015, 85.1316546),
+    host: const MUser(
+        id: '2', name: 'Kien Vo', avatar: "assets/images/images/avatar.png"),
+    follower: 122,
+    type: 'sport',
+  ),
+  MEvent(
+    id: '3',
+    name: "Sunday",
+    description: "this is description",
+    images: ["assets/images/images/bg-event.jpg"],
+    startDate: DateTime.now(),
+    deadline: DateTime.now(),
+    province: 'Ho Chi Minh',
+    location: LatLng(88.00015, 85.1316546),
+    host: const MUser(
+        id: '2', name: 'Kien Vo', avatar: "assets/images/images/avatar.png"),
+    follower: 122,
+    type: 'sport',
+  ),
+  MEvent(
+    id: '4',
+    name: "Merry",
+    description: "this is description",
+    images: ["assets/images/images/bg-event.jpg"],
+    startDate: DateTime.now(),
+    deadline: DateTime.now(),
+    province: 'Ho Chi Minh',
+    location: LatLng(88.00015, 85.1316546),
+    host: const MUser(
+        id: '2', name: 'Kien Vo', avatar: "assets/images/images/avatar.png"),
+    follower: 122,
+    type: 'sport',
+  ),
+];
 
 class EventRepositoryMock {
   MResult<MEvent> getEvent(String id) {
     final MEvent result = MEvent.ds(id: '1', host: MUser.empty());
+    return MResult.success(result);
+  }
+
+  MResult<List<MEvent>> getEventsSearch(String search) {
+    if (search == "") return MResult.success([]);
+    final result = events
+        .where((element) =>
+            element.name.toLowerCase().contains(search.toLowerCase()))
+        .toList();
+
     return MResult.success(result);
   }
 
