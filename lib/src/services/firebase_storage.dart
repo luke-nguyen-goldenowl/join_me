a10a8d7bd3550d653f9dcd261c7178323d4c9250
Keith-Go <152830147+Keith-Go@users.noreply.github.com>
2024-01-06 22:44:44 +0700

    update name class firebaseStorage

--- lib/src/services/firebase_storage.dart
+++ lib/src/services/firebase_storage.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart';
 
-class MFirebaseStorage {
+class XFirebaseStorage {
   final FirebaseStorage _storage = FirebaseStorage.instance;
 
   Future<String> uploadImage(String imagePath, String folderName) async {
