13697828dd2c27de41670c582ff324f2168cfac0
kienvo3105 <keith.vo.goldenowl@gmail.com>
2024-01-06 22:46:05 +0700

    create class firebase storage

--- lib/src/network/data/story/story_reference.dart
+++ lib/src/network/data/story/story_reference.dart
@@ -1,9 +1,8 @@
-import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:myapp/src/network/firebase/base_collection.dart';
 import 'package:myapp/src/network/model/common/result.dart';
 import 'package:myapp/src/network/model/story/story.dart';
+import 'package:myapp/src/services/firebase_storage.dart';
 
 class StoryReference extends BaseCollectionReference<MStory> {
   StoryReference()
@@ -19,29 +18,11 @@ class StoryReference extends BaseCollectionReference<MStory> {
           setObjectId: (e, id) => e.copyWith(id: id),
         );
 
-  Future<String> _uploadImage(String imagePath) async {
-    try {
-      DateTime now = DateTime.now();
-
-      String imageName = 'image_${now.microsecondsSinceEpoch}.jpg';
-
-      Reference ref = FirebaseStorage.instance.ref('stories').child(imageName);
-
-      Future<String> uploadTask =
-          await ref.putFile(File(imagePath)).then((taskSnapshot) async {
-        return taskSnapshot.ref.getDownloadURL();
-      });
-
-      return uploadTask;
-    } catch (e) {
-      print('Lỗi khi tải ảnh lên Firebase Storage: $e');
-      return "";
-    }
-  }
+  final XFirebaseStorage firebaseStorage = XFirebaseStorage();
 
   Future<MResult<MStory>> addStory(MStory story) async {
     try {
-      final image = await _uploadImage(story.image!);
+      final image = await firebaseStorage.uploadImage(story.image!, "stories");
       MStory newStory = story.copyWith(image: image);
       final MResult<MStory> result = await add(newStory);
       return MResult.success(result.data);
