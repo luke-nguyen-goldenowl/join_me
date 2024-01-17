50292fad09256ba33577c41e1845ec0537949930
kienvo3105 <keith.vo.goldenowl@gmail.com>
2024-01-17 09:08:51 +0700

    delete repo mock

--- lib/src/network/domain_manager.dart
+++ lib/src/network/domain_manager.dart
@@ -1,10 +1,7 @@
 import 'package:myapp/src/network/data/event/event_repository_impl.dart';
-import 'package:myapp/src/network/data/event/event_repository_mock.dart';
 import 'package:myapp/src/network/data/notification/notification_repository_impl.dart';
 import 'package:myapp/src/network/data/sign/sign_repository_impl.dart';
 import 'package:myapp/src/network/data/story/story_repository_impl.dart';
-import 'package:myapp/src/network/data/user/user_repository_mock.dart';
-import 'package:myapp/src/network/data/story/story_repository_mock.dart';
 import 'package:myapp/src/network/data/user_story/user_story_repository_impl.dart';
 import 'blob/data/upload_repository_impl.dart';
 import 'data/user/user_repository_impl.dart';
@@ -20,11 +17,8 @@ class DomainManager {
   final user = UserRepositoryImpl();
   final upload = UploadRepositoryImpl();
   final sign = SignRepositoryImpl();
-  final storyMock = StoryRepositoryMock();
   final story = StoryRepositoryImpl();
-  final eventMock = EventRepositoryMock();
   final event = EventRepositoryImpl();
-  final userMock = UserRepositoryMock();
   final userEvent = UserStoryRepositoryImpl();
   final notification = NotificationRepositoryImpl();
 }
